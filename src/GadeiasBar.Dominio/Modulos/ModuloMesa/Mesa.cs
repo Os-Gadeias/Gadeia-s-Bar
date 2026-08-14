5029368f6f8721c24afd210bf98aefbadc1cd95e
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-08-14 15:57:57 -0300

    ADD casse Mesa e Enum StatusMesa

--- src/GadeiasBar.Dominio/Modulos/ModuloMesa/Mesa.cs
+++ src/GadeiasBar.Dominio/Modulos/ModuloMesa/Mesa.cs
@@ -1,6 +1,51 @@
+using GadeiasBar.Dominio.Compartilhado;
+
 namespace GadeiasBar.Dominio.Modulos.ModuloMesa;
 
-public class Mesa
+public class Mesa : EntidadeBase<Mesa>
 {
+    public int NumeroMesa { get; set; } = 0;
+    public int QuantidadeLugares { get; set; } = 0;
+    public StatusMesa statusMesa { get; set; }
 
+    public Mesa() { }
+
+    public Mesa(int numeroMesa, int quantidadeLugares, StatusMesa statusMesa)
+    {
+        NumeroMesa = numeroMesa;
+        QuantidadeLugares = quantidadeLugares;
+        this.statusMesa = statusMesa;
+    }
+
+    public override void Atualizar(Mesa entidadeAtualizada)
+    {
+        NumeroMesa = entidadeAtualizada.NumeroMesa;
+        QuantidadeLugares = entidadeAtualizada.QuantidadeLugares;
+        statusMesa = entidadeAtualizada.statusMesa;
+    }
+
+    public override List<string> Validar()
+    {
+        List<string> erros = new List<string>();
+
+        if (NumeroMesa == 0)
+            erros.Add("O campo \"Numero Da Mesa\" deve ser preenchido");
+
+        else if (NumeroMesa < 0)
+            erros.Add("O campo \"Numero Da Mesa\" não pode ser um numero negativo");
+
+        if (QuantidadeLugares == 0)
+            erros.Add("O campo \"Quantidade De Lugares\" deve ser preenchido");
+
+        else if (QuantidadeLugares < 0)
+            erros.Add("O campo \"Quantidade De Lugares\" não pode ser um numero negativo");
+
+        else if (QuantidadeLugares > 10)
+            erros.Add("O campo \"Quantidade De Lugares\" não pode ultrapassar 10 lugares");
+
+        if (!Enum.IsDefined(typeof(StatusMesa), statusMesa))
+            erros.Add("O campo \"Status Da Mesa\" é inválido");
+
+        return erros;
+    }
 }
---
+++ src/GadeiasBar.Dominio/Modulos/ModuloMesa/StatusMesa.cs
@@ -0,0 +1,7 @@
+namespace GadeiasBar.Dominio.Modulos.ModuloMesa;
+
+public enum StatusMesa
+{
+    Livre = 0,
+    Ocupada = 1
+}
