ede68f40265b04afb3ce5dc6ae8f3a97161b4997
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-08-16 14:27:09 -0300

    Adicionar classe ServicoMesa e métodos para operações de mesa; implementar mapeamento no MapperMesa

---
+++ src/GadeiasBar.WebApp/Modulos/ModuloMesa/Mapping/MapperMesa.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+
+namespace GadeiasBar.WebApp.Modulos.ModuloMesa.Mapping;
+
+public class MapperMesa : Profile
+{
+    public MapperMesa()
+    {
+        CreateMap<ListarMesaDto, ListarMesaViewModel>();
+        CreateMap<CadastrarMesaViewModel, CadastrarMesaDto>();
+        CreateMap<EditarMesaViewModel, EditarMesaDto>();
+        CreateMap<ListarMesaDto, EditarMesaViewModel>();
+        CreateMap<ExcluirMesaDto, ExcluirMesaViewModel>();
+    }
+}
--- src/GadeiasBar.WebApp/Modulos/ModuloMesa/MesaController.cs
+++ src/GadeiasBar.WebApp/Modulos/ModuloMesa/MesaController.cs
@@ -1,12 +1,42 @@
+using AutoMapper;
+using FluentResults;
+using GadeiasBar.Aplicacao.Modulos.ModuloMesa;
+using GadeiasBar.WebApp.Compartilhado.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GadeiasBar.WebApp.Modulos.ModuloMesa;
 
-public class MesaController : Controller
+public class MesaController(IMapper mapper, ServicoMesa servicoMesa) : Controller
 {
     [HttpGet]
     public ActionResult Listar()
     {
+        List<ListarMesaDto> dtos = servicoMesa.SelecionarTodos();
+        List<ListarMesaViewModel> listarVm = mapper.Map<List<ListarMesaViewModel>>(dtos);
+        return View(listarVm);
+    }
+
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
         return View();
+    }
+
+    [HttpPost]
+    public ActionResult Cadastrar(CadastrarMesaViewModel vm)
+    {
+        if (!ModelState.IsValid)
+            return View(vm);
+
+        CadastrarMesaDto dto = mapper.Map<CadastrarMesaDto>(vm);
+        Result result = servicoMesa.Cadastrar(dto);
+
+        if (result.IsFailed)
+        {
+            ModelState.AddModelError(result);
+            return View(vm);
+        }
+
+        return RedirectToAction(nameof(Listar));
     }
 }
