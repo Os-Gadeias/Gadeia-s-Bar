962ef84e573064f0c31b596f1b9dbc2375090904
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-08-18 01:13:47 -0300

    corrige o namespace da classe RepositorioPedidoEmOrm para ModuloPedido

--- src/GadeiasBar.Infra/Modulos/ModuloPedido/RepositorioPedido.cs
+++ src/GadeiasBar.Infra/Modulos/ModuloPedido/RepositorioPedido.cs
@@ -2,7 +2,7 @@ using GadeiasBar.Dominio.Modulos.ModuloMesa;
 using GadeiasBar.Dominio.Modulos.ModuloPedido;
 using GadeiasBar.Infra.Compartilhado.Orm;
 
-namespace GadeiasBar.Infra.Modulos.ModuloMesa;
+namespace GadeiasBar.Infra.Modulos.ModuloPedido;
 
 public class RepositorioPedidoEmOrm(GadeiasBarDbContext dbContext)
 : RepositorioBaseEmOrm<Pedido>(dbContext), IRepositorioPedido;
