d4de8e0871478818933067765e418ad5a1feddc5
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-08-18 01:09:40 -0300

    implementa construtor e métodos de atualização e validação na classe Pedido

--- src/GadeiasBar.Dominio/Modulos/ModuloPedido/Pedido.cs
+++ src/GadeiasBar.Dominio/Modulos/ModuloPedido/Pedido.cs
@@ -7,16 +7,36 @@ namespace GadeiasBar.Dominio.Modulos.ModuloPedido;
 public class Pedido : EntidadeBase<Pedido>, IEntidadeDoUsuario
 {
     public Produto Produto { get; set; } = null!;
-    public int Quantidade { get; set; }
+    public int Quantidade { get; set; } = 0;
     public Guid UserId { get; set; }
+
+    public Pedido() { }
+
+    public Pedido(Produto produto, int quantidade, Guid userId)
+    {
+        Produto = produto;
+        Quantidade = quantidade;
+    }
 
     public override void Atualizar(Pedido entidadeAtualizada)
     {
-        throw new NotImplementedException();
+        Produto = entidadeAtualizada.Produto;
+        Quantidade = entidadeAtualizada.Quantidade;
     }
 
     public override List<string> Validar()
     {
-        throw new NotImplementedException();
+        List<string> erros = new List<string>();
+
+        if (Quantidade == 0)
+            erros.Add("O campo \"Quantidae\" n pode sem 0");
+
+        else if (Quantidade < 0)
+            erros.Add("O campo \"Quantidade\" n pode ser um numero negativo");
+
+        if (Produto == null)
+            erros.Add("O campo \"Produto\" deve ser preenchido");
+
+        return erros;
     }
 }
