f10fa40e713cbc476a3c6ccde607864a5bc8ea34
thig0S_ <thiagokovalski5@gmail.com>
2026-08-19 15:59:49 -0300

    CT-CONT-U-002 add

--- tests/GadeiasBar.Testes.Unidade/Modulos/ModuloConta/ServicoContaTest.cs
+++ tests/GadeiasBar.Testes.Unidade/Modulos/ModuloConta/ServicoContaTest.cs
@@ -38,5 +38,36 @@ public class ServicoContaTest
 
         Assert.IsTrue(resultado.IsSuccess);
         Assert.IsNotNull(contaCadastrada);
+        repositorioConta.Verify(r => r.Cadastrar(It.IsAny<Conta>()), Times.Once);
+    }
+    [TestMethod]
+    public void CadastrarConta_SemNomeClienteValido_RetornaErro()
+    {
+        Mock<IRepositorioMesa> repositorioMesa = new();
+        Mock<IRepositorioConta> repositorioConta = new();
+        Mock<IRepositorioGarcom> repositorioGarcom = new();
+
+        ServicoConta servicoConta = new(
+            repositorioConta.Object,
+            repositorioMesa.Object,
+            repositorioGarcom.Object);
+
+        Mesa mesa = new();
+        Garcom garcom = new();
+
+        repositorioMesa.Setup(r => r.SelecionarPorId(It.IsAny<Guid>())).Returns(mesa);
+        repositorioGarcom.Setup(r => r.SelecionarPorId(It.IsAny<Guid>())).Returns(garcom);
+
+        Conta? contaCadastrada = null!;
+        repositorioConta.Setup(c => c.Cadastrar(It.IsAny<Conta>())).Callback<Conta>(conta => contaCadastrada = conta);
+
+        Result resultado = servicoConta.Cadastrar(
+            new(string.Empty,
+            mesa.Id.ToString(),
+            garcom.Id.ToString()));
+
+        Assert.IsTrue(resultado.IsFailed);
+        Assert.IsNull(contaCadastrada);
+        repositorioConta.Verify(r => r.Cadastrar(It.IsAny<Conta>()), Times.Never);
     }
 }
