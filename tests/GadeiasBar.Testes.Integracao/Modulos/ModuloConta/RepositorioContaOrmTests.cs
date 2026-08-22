1d3155cb115104bf6d5c995933cd796b06171873
thig0S_ <thiagokovalski5@gmail.com>
2026-08-21 22:08:12 -0300

    add verificações de Crud no repositorio

--- tests/GadeiasBar.Testes.Integracao/Modulos/ModuloConta/RepositorioContaOrmTests.cs
+++ tests/GadeiasBar.Testes.Integracao/Modulos/ModuloConta/RepositorioContaOrmTests.cs
@@ -24,4 +24,70 @@ public class RepositorioContaOrmTests : RepositorioBaseEmOrmTests
         Assert.IsNotNull(contaCadastrada);
         Assert.AreEqual("Thiago", contaCadastrada.NomeCliente);
     }
+    [TestMethod]
+    public void EditarConta_ComDadosValidos_Altera()
+    {
+        Mesa mesa = new();
+        Garcom garcom = new();
+
+        Conta conta = new("Thiago", garcom, mesa);
+
+        repositorioConta.Cadastrar(conta);
+        dbContext.ChangeTracker.Clear();
+
+        Conta? contaAtualizada = repositorioConta.SelecionarPorId(conta.Id);
+
+        contaAtualizada!.NomeCliente = "Thiago Kovalski";
+
+        repositorioConta.Editar(conta.Id, contaAtualizada);
+        dbContext.ChangeTracker.Clear();
+
+        Conta? contaEditada = repositorioConta.SelecionarPorId(conta.Id);
+
+        Assert.IsNotNull(contaEditada);
+        Assert.AreEqual("Thiago Kovalski", contaEditada.NomeCliente);
+    }
+    [TestMethod]
+    public void ExcluirConta_ComContaExistente_Remove()
+    {
+        Mesa mesa = new();
+        Garcom garcom = new();
+
+        Conta conta = new("Thiago", garcom, mesa);
+
+        repositorioConta.Cadastrar(conta);
+        dbContext.ChangeTracker.Clear();
+
+        Conta? contaCadastrada = repositorioConta.SelecionarPorId(conta.Id);
+
+        Assert.IsNotNull(contaCadastrada);
+
+        repositorioConta.Excluir(conta.Id);
+        dbContext.ChangeTracker.Clear();
+
+        Conta? contaExcluida = repositorioConta.SelecionarPorId(conta.Id);
+
+        Assert.IsNull(contaExcluida);
+    }
+    [TestMethod]
+    public void SelecionarTodos_ComDuasContas_RetornaDuasContas()
+    {
+        Mesa mesa1 = new();
+        Garcom garcom1 = new();
+
+        Mesa mesa2 = new();
+        Garcom garcom2 = new();
+
+        Conta conta1 = new("Thiago", garcom1, mesa1);
+        Conta conta2 = new("Victor", garcom2, mesa2);
+
+        repositorioConta.Cadastrar(conta1);
+        repositorioConta.Cadastrar(conta2);
+
+        dbContext.ChangeTracker.Clear();
+
+        List<Conta> contas = repositorioConta.SelecionarTodos();
+
+        Assert.AreEqual(2, contas.Count);
+    }
 }
